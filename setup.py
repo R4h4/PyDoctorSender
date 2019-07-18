b9d47d0d46dff5250f0d3cd77bc65a7d4965180d
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2019-07-18 14:57:20 +0700

    Prep for 0.9 release

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pydoctorsender',
   packages = ['pydoctorsender'],
-  version = '0.8',
+  version = '0.9',
   license='MIT',
   description = 'An unofficial API wrapper for DoctorSender',
   author = 'Karsten Eckhardt',
   author_email = 'karsten.eckhardt@gmail.com',
   url = 'https://github.com/r4h4/PyDoctorSender',
-  download_url = 'https://github.com/r4h4/PyDoctorSender/archive/0.8.tar.gz',
+  download_url = 'https://github.com/r4h4/PyDoctorSender/archive/0.9.tar.gz',
   keywords = ['doctorsender', 'email', 'marketing', 'api'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests'
