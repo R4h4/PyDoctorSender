bd8ccb83368d12fbe8658e8733493f6493411efd
Karsten Eckhardt <karsten.eckhardt@gmail.com>
2020-10-01 18:36:01 +0700

    Increased default timeout time because Doctorsender is slow unreliable

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pydoctorsender',
   packages = ['pydoctorsender'],
-  version = '0.17',
+  version = '0.18',
   license='MIT',
   description = 'An unofficial API wrapper for DoctorSender',
   author = 'Karsten Eckhardt',
   author_email = 'karsten.eckhardt@gmail.com',
   url = 'https://github.com/r4h4/PyDoctorSender',
-  download_url = 'https://github.com/r4h4/PyDoctorSender/archive/v0.17.tar.gz',
+  download_url = 'https://github.com/r4h4/PyDoctorSender/archive/v0.18.tar.gz',
   keywords = ['doctorsender', 'email', 'marketing', 'api'],   # Keywords that define your package best
   install_requires=[
           'requests'
